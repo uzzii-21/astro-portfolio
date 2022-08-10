4a387fa4de93c83873283ba434001890eb889902
Uzair Ahmed <uzairahmed74372@gmail.com>
2022-08-10 11:33:26 +0500

    integrated tailwind css and created folder structure

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class", '[data-mode="dark"]'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#111111",
+        light: "#FFFFFF",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        outfit: ["Outfit", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
