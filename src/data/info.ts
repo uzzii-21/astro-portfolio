1421668f27708f243a28c20ca4d22942d5af9490
Uzair Ahmed <uzairahmed74372@gmail.com>
2022-08-23 19:08:20 +0500

    resolve absolute path issue

---
+++ src/data/info.ts
@@ -0,0 +1,11 @@
+export const info = {
+  name: "Uzair Ahmed",
+  jobDescription: "Web Developer",
+  about: `I'm a software engineer based in Toronto, Canada and also a
+  communication and journalism student. I enjoy creating things that live
+  on the internet, whether that be websites, applications, or anything in
+  between. I have been freelancing for a year now while studying at the
+  university and I've manage to gain a decent amount of experience and
+  valuable knowledge from all different kinds of fields throughout my
+  projects/work.`,
+};
--- src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-const createLogo = (name) =>
-  name
-    .split(" ")
-    .map((word) => word[0])
-    .join("")
-    .toUpperCase();
-
-export { createLogo };
