595af6577345897d70e8318b213891c2fe05254d
Uzair Ahmed <uzairahmed74372@gmail.com>
2022-09-03 23:25:22 +0500

    fixed type issue

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export interface IPageTitle {
+  title: string;
+}
+
+export interface IHeroProps {
+  name: string;
+  about: string;
+}
+
+export interface IExperience {
+  name: string;
+  location: string;
+  startDate: string;
+  endDate: string;
+  description: string[];
+}
+export interface IExperiences {
+  title: string;
+  details: IExperience[];
+}
