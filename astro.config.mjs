03502db72ae4008707b5a097aa16134d2383eb15
Uzair Ahmed <uzairahmed74372@gmail.com>
2022-09-05 02:16:45 +0500

    Integrate sitemap and robotsTxt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://astro-portfolio-uzair.vercel.app",
+  integrations: [
+    tailwind(),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date(),
+      entryLimit: 10000,
+    }),
+    robotsTxt(),
+  ],
 });
