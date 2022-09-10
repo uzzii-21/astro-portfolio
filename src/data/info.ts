c9e990dca68dc1a607bf4a7321528d590bb6de95
Uzair Ahmed <uzairahmed74372@gmail.com>
2022-09-10 23:45:57 +0500

    update personal data

--- src/data/info.ts
+++ src/data/info.ts
@@ -2,11 +2,9 @@ export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
   name: "Uzair Ahmed",
   jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Toronto, Canada and also a
-  communication and journalism student. I enjoy creating things that live
+  about: `I'm a software engineer based in Karachi, Pakistan. I enjoy creating things that live
   on the internet, whether that be websites, applications, or anything in
-  between. I have been freelancing for a year now while studying at the
-  university and I've manage to gain a decent amount of experience and
+  between. I have been working for 3 year, I've manage to gain a decent amount of experience and
   valuable knowledge from all different kinds of fields throughout my
   projects/work.`,
 
