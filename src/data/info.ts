7caed9c4de337e8fe3316c9f5e111ec097ad7ced
Uzair Ahmed <uzairahmed74372@gmail.com>
2022-09-13 17:20:43 +0500

    education updated

--- src/data/info.ts
+++ src/data/info.ts
@@ -14,45 +14,66 @@ export const info = {
 
   experience: [
     {
-      name: "Internative Labs",
-      location: " karachi",
-      startDate: "Jul 2021",
+      name: "InterNative Labs, LLC",
+      location: "Karāchi, Sindh, Pakistan",
+      startDate: "Apr 2022",
       endDate: "Present",
       description: [
         "- Developing screens and UI components for the web application using React and Tailwind.",
         "- Fixing UI issues and integrating backend APIs with Redux Saga.",
       ],
     },
+
+    {
+      name: "Coupon Approve (Remote)",
+      location: "Amsterdam, North Holland, Netherlands",
+      startDate: "Nov 2020",
+      endDate: "Apr 2022",
+      description: [
+        "✓ Developing code in accordance with appropriate design and architectural rules",
+        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
+        "✓ Help and training of other colleagues",
+        "✓ Others tasks connected to the nature of the job",
+        "✓ Code Improvements and Refactoring ",
+        "✓ Best Practices and Rules Maintainer",
+      ],
+    },
+
+    {
+      name: "Interns Pakistan",
+      location: "Pakistan",
+      startDate: "Oct 2020",
+      endDate: "Nov 2020",
+      description: [
+        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
+        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+      ],
+    },
   ],
 
   education: [
     {
-      name: "University of Toronto",
-      location: "Canada",
-      startDate: "2018",
-      endDate: "2021",
-      description: ["- Bachelor's degree in communication"],
+      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
+      location: "Karāchi, Sindh, Pakistan",
+      startDate: "2019",
+      endDate: "2022",
+      description: [
+        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+      ],
     },
     {
-      name: "University of Toronto",
-      location: "Canada",
+      name: "Govt. Degree Boys College Buffer Zone",
+      location: "Karāchi, Sindh, Pakistan",
       startDate: "2018",
-      endDate: "2021",
-      description: ["- Bachelor's degree in communication"],
+      endDate: "2020",
+      description: ["Intermediate, Pre-Engineering"],
     },
     {
-      name: "University of Toronto",
-      location: "Canada",
-      startDate: "2018",
-      endDate: "2021",
-      description: ["- Bachelor's degree in communication"],
-    },
-    {
-      name: "University of Toronto",
-      location: "Canada",
-      startDate: "2018",
-      endDate: "2021",
-      description: ["- Bachelor's degree in communication"],
+      name: "Ghazi Foundation Schools & Colleges",
+      location: "Karāchi, Sindh, Pakistan",
+      startDate: "2016",
+      endDate: "2018",
+      description: ["Matriculation,  Computer Science"],
     },
   ],
   socialMedia: {
