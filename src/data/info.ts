b98df1e1eed128125fc46b94d94fe814391602ea
Uzair Ahmed <uzairahmed74372@gmail.com>
2022-09-13 17:02:03 +0500

    about updated

--- src/data/info.ts
+++ src/data/info.ts
@@ -2,11 +2,15 @@ export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
   name: "Uzair Ahmed",
   jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan. I enjoy creating things that live
-  on the internet, whether that be websites, applications, or anything in
-  between. I have been working for 3 year, I've manage to gain a decent amount of experience and
-  valuable knowledge from all different kinds of fields throughout my
-  projects/work.`,
+  about: `I'm a software engineer based in Karachi, Pakistan.
+   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
+   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
+   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
+   I have successfully completed several web projects from scratch. 
+   Yet, I learn continuously and never stop exploring new technologies.
+   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
+   firebase and React Native with time.
+  `,
 
   experience: [
     {
