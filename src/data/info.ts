3439f15078dcce17a238adde0726414c3ec6813c
dinxsh <16219421127@atmiyauni.edu.in>
2023-03-09 13:38:26 +0530

    update

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,51 +1,48 @@
 export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Uzair Ahmed",
+  name: "Dinesh Talwadker",
   jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
+  about: `I'm a software engineer based in India.
    I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
    I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
+   I have 3 years of experience in Full Stack Web Development using MERN, MEAN, Astro, Nextjs and more. Hence,
+   I have successfully completed several projects from scratch. 
    Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
   `,
 
   experience: [
     {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
+      name: "Znotes",
+      location: "Remote",
+      startDate: "Jan 2023",
       endDate: "Present",
       description: [
-        "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+        "- Developing screens and UI components for the web application using React, MongoDb and Storybook.",
+        "- Worked on new and existing features for znotes discord bot built in discord.js.",
       ],
     },
 
     {
-      name: "Coupon Approve (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
+      name: "Freelancer @ Fiverr",
+      location: "Remote",
+      startDate: "Aug 2022",
+      endDate: "Present",
       description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
+        "- Worked with multiple international clients to deliver quality software.",
+        "- Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions.",
+        "- Delivered all orders on time.",
+        "- Others tasks connected to the nature of the job.",
+        "- Code Improvements and Refactoring.",
       ],
     },
 
     {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
+      name: "Krishn Infotech",
+      location: "India",
+      startDate: "Jan 2021",
+      endDate: "March 2022",
       description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
+        "An Immersive Internship Program that prepares me for the world's most in-demand career. The program is completely based on learning and challenges.",
         "The internship track is built by an industry expert that let develop the skills in the areas most important ",
       ],
     },
@@ -53,45 +50,20 @@ export const info = {
 
   education: [
     {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
+      name: "Atmiya Institute of Technology and Science",
+      location: "Gujarat, India",
       startDate: "2019",
       endDate: "2022",
       description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+        "Diploma in Computer Engineering",
       ],
-    },
-    {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2018",
-      endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
-    },
-    {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2016",
-      endDate: "2018",
-      description: ["Matriculation,  Computer Science"],
     },
   ],
 
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
-    twitter: "https://twitter.com/Uzzii_21",
-    github: "https://github.com/uzzii-21",
-    email: "uzairahmed@74372gmail.com",
-    linkedin: "https://www.linkedin.com/in/uzzii21/",
+    twitter: "https://twitter.com/dineshcodes",
+    github: "https://github.com/dinxsh",
+    email: "dineshtalwadker@gmail.com",
+    linkedin: "https://www.linkedin.com/in/supdinesh/",
   },
-
-  projects: [
-    {
-      title: "Astro Portfolio",
-      isFeatured: true,
-      thumbnail: "/assets/images/astro-portfolio.png",
-      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
-      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
-    },
-  ],
 };
