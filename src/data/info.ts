4eb2f607a5e97c82ba01065578b3576c84f9e294
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2023-06-17 04:52:39 +0000

    Updated with my own data

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,88 +1,73 @@
 export const info = {
-  baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Uzair Ahmed",
+  baseUrl: "https://olivo.moe/",
+  name: "Edwin Olivo",
   jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
+  about: `Soy ingeniero de software y vivo en Tepic, México.
+    Recién egresado de la universidad con una especialización en desarrollo web.
+    Aunque no tengo experiencia laboral previa, cuento con conocimientos teóricos y prácticos sobre los lenguajes de programación y herramientas relacionadas con el desarrollo web.
+    Además, tengo la capacidad de utilizar mi formación y creatividad para resolver problemas, y trabajar en equipo para lograr objetivos.
   `,
 
   experience: [
     {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
-      endDate: "Present",
+      name: "CICESE-UT3",
+      position: "Becario",
+      location: "Tepic, Nayarit, México",
+      startDate: "Septiembre 2022",
+      endDate: "Diciembre 2022",
       description: [
-        "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+        "Responsable de desarrollar parte de la `interfaz web` para el nuevo sistema de gestión de inventario. Empleando `Pug` para componentes y *layouts*, y `Vue` para añadir funcionalidad.",
+        "Codificación del *end points* para la aplicación usando `Express` y `TypeScript`.",
+        "Encargado de la `migración` y `actualización` base de datos. Detección de registros duplicados; eliminación de dependencias incoherentes; corrección de código de caracteres; creación nuevas tablas para el nuevo modelo; y reorganización de los campos derivados."
       ],
     },
 
     {
-      name: "Coupon Approve (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
+      name: "D4ALL",
+      position: "Editor de contenidos",
+      location: "Tepic, Nayarit, México",
+      startDate: "Junio 2022",
+      endDate: "Agosto 2022",
       description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
+        "Trasladar el contenido (texto, tablas e imágenes) del sitio web antiguo al nuevo seleccionando el componente de interfaz adecuado para cada instancia.",
+        "Colaboración con el equipo de desarrollo y diseño en la detección de errores e inconsistencias en los componentes Drupal.",
+        "Empleé `Jira` para el control de las asignaciones o *tickets*.",
       ],
     },
 
     {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
+      name: "Unidad Académica de Economía",
+      position: "Porgramador",
+      location: "Tepic, Nayarit, México",
+      startDate: "Diciembre 2019",
+      endDate: "Marzo 2020",
       description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+        "Colaboré de manera productiva en equipo para conocer los requisitos y las especificaciones en torno a la gestión de historiales médicos.",
+        "Codifiqué el módulo de odontología con Java Swing para el registro de las revisiones dentales.",
+        "Participe activamente en el diseño de funcionalidades para cada especialidad médica.",
+        "Encargado de garantizar la aplicación de las reglas de normalización en la base de datos."
       ],
     },
   ],
 
   education: [
     {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2019",
-      endDate: "2022",
+      name: "Universidad Autónoma de Nayarit",
+      location: "Tepic, Nayarit, México",
+      startDate: "2017",
+      endDate: "2023",
       description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+        "Licenciatura en Sistemas Computacionales",
       ],
-    },
-    {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2018",
-      endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
-    },
-    {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2016",
-      endDate: "2018",
-      description: ["Matriculation,  Computer Science"],
     },
   ],
 
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
-    twitter: "https://twitter.com/Uzzii_21",
-    github: "https://github.com/uzzii-21",
-    email: "uzairahmed@74372gmail.com",
-    linkedin: "https://www.linkedin.com/in/uzzii21/",
+    facebook: "",
+    twitter: "",
+    github: "https://github.com/MrOlivo",
+    email: "",
+    linkedin: "https://www.linkedin.com/in/edwin-olivo-garcia/",
   },
 
   projects: [
