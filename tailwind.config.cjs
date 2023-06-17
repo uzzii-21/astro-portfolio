c02ef50346999ad046d961ce9187427246d0e78c
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2023-06-17 04:50:07 +0000

    added custom list item

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,6 +15,9 @@ module.exports = {
         inter: ["Inter", "sans-serif"],
         outfit: ["Outfit", "sans-serif"],
       },
+      listStyleImage: {
+        checkmark: "url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTQiIGhlaWdodD0iMTIiIHZpZXdCb3g9IjAgMCAxNCAxMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiBmaWxsPSIjMzhiZGY4Ij48cGF0aCBmaWxsLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMy42ODUuMTUzYS43NTIuNzUyIDAgMCAxIC4xNDMgMS4wNTJsLTggMTAuNWEuNzUuNzUgMCAwIDEtMS4xMjcuMDc1bC00LjUtNC41YS43NS43NSAwIDAgMSAxLjA2LTEuMDZsMy44OTQgMy44OTMgNy40OC05LjgxN2EuNzUuNzUgMCAwIDEgMS4wNS0uMTQzWiIgLz48L3N2Zz4=')",
+      }
     },
   },
   plugins: [],
